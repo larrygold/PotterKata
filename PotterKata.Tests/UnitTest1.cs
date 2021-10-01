f3c798542613f8d3a696eb671fd691f9af195aaa
Larry Goldstein <larry.goldstein@upslide.net>
2021-10-01 19:01:40 +0200

    Up to 5 books

--- PotterKata.Tests/UnitTest1.cs
+++ PotterKata.Tests/UnitTest1.cs
@@ -9,13 +9,24 @@ namespace PotterKata.Tests
         {
         }
 
-        [TestCase(new int[] { 0 }, 8)]
-        [TestCase(new int[] {0, 1}, 15.2)]
-        [TestCase(new int[] {0, 1, 2}, 21.6)]
+
+        [TestCase(new int[] { 0 }, 8, TestName = "1 book")]
+        [TestCase(new int[] {0, 1}, 15.2, TestName = "2 books")]
+        [TestCase(new int[] {0, 1, 2}, 21.6, TestName = "3 books")]
+        [TestCase(new int[] {0, 1, 2, 3}, 25.6, TestName = "4 books")]
+        [TestCase(new int[] {0, 1, 2, 3, 4}, 30, TestName = "5 books")]
         public void NoBookDuplicate(int[] books, double expected)
         {
             Assert.AreEqual(expected, new Basket().GetPrice(books));
         }
+
+        [Ignore("")]
+        [TestCase(new int[] { 0, 0 }, 16, TestName = "2 identical books")]
+        public void BookDuplicates(int[] books, double expected)
+        {
+            Assert.AreEqual(expected, new Basket().GetPrice(books));
+        }
+
 
 
     }
--- PotterKata/Basket.cs
+++ PotterKata/Basket.cs
@@ -8,13 +8,19 @@ namespace PotterKata
         {
             switch (books.Length)
             {
+                case 1:
+                    return 8.0;
                 case 2:
                     return 15.2;
                 case 3:
                     return 21.6;
+                case 4:
+                    return 25.6;
+                case 5:
+                    return 30.0;
+                default:
+                    throw new Exception();
             }
-                
-            return 8.0;
         }
     }
 }
